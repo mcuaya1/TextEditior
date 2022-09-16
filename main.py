f1d8f1bc8a55ad4d2c1c8eb8e39eeaad3d7f524b
mcuaya1 <yuhuntter@gmail.com>
2022-09-15 21:53:49 -0700

    PythonText Editor Version 1.0.0

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+from tkinter import *
+from tkinter.filedialog import asksaveasfile
+from tkinter.filedialog import askopenfile
+from tkinter.messagebox import showerror
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+filename = None
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def newFile():
+    global filename
+    filename = "Untitled"
+    text.delete(0.0, END)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def saveFile():
+    global filename
+    t = text.get(0.0, END)
+    f = open(filename, 'w')
+    f.write(t)
+    f.close()
+
+
+def saveFileAs():
+    f = asksaveasfile(filetypes=[('Text File','*.txt')], defaultextension='.txt', title="Save As")
+    t = text.get(0.0, END)
+    try:
+        f.write(t.rstrip())
+    except:
+        showerror(title="Oops...", message="Unable to save file...")
+
+
+root = Tk()
+
+
+def openFile():
+    global filename
+    file = askopenfile(parent=root, title='Select a File')
+    filename = file.name
+    t = file.read()
+    text.delete(0.0, END)
+    text.insert(0.0, t)
+    file.close()
+
+root.title("PythonText Editor(1.0.0)")
+root.minsize(width=400, height=400)
+root.maxsize(width=400, height=400)
+
+text = Text(root, width=400, height=400)
+text.pack()
+
+menubar = Menu(root)
+filemenu = Menu(menubar)
+filemenu.add_command(label="New", command=newFile)
+filemenu.add_command(label="Open", command=openFile)
+filemenu.add_command(label="Save", command=saveFile)
+filemenu.add_command(label="Save As...", command=saveFileAs)
+filemenu.add_separator()
+filemenu.add_command(label="Quit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+root.config(menu=filemenu)
+root.mainloop()
